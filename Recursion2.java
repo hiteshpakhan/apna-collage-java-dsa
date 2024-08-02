a598231fdea9c03857bc2e6db69ee4dc523a0ed8
hiteshpakhan <hitesh5pakhan@gmail.com>
2024-08-02 19:05:18 +0530

    a

--- Recursion2.java
+++ Recursion2.java
@@ -47,3 +47,74 @@
 
 //-------------------------------------------------
 
+//to sum of n natural numbers
+// public class Recursion2{
+
+//     public static void printsum(int i, int n, int sum) {
+//         if(i == n){
+//             sum += i;
+//             System.out.println(sum);
+//             return;
+//         }
+//         sum += i;
+//         printsum(i+1, n, sum);
+//     }
+//     public static void main(String args[]){
+//         printsum(1, 5, 0);
+//     }
+// }
+
+
+// //-------------------------------------------------
+// // print factorial of a number n
+// public class Recursion2{
+
+//     public static int calfactorial(int n) {
+//         if(n == 1 || n == 0){
+//             return 1;
+//         }
+
+//         int fact_nm1 = calfactorial(n-1);
+//         int fact_n = n * fact_nm1;
+//         return fact_n;
+
+//     }
+//     public static void main(String args[]){
+//         int n = 5;
+//         int ans = calfactorial(n);
+//         System.out.println(ans);
+//     }
+// }
+
+
+// // -------------------------------------------------
+// // print the febnaci sequence till nth term
+// public class Recursion2{
+//     public static void printFib(int a, int b, int n) {
+//         if(n == 0){
+//             return;
+//         }
+//         int c = a + b;
+//         System.out.print(c+",");
+//         printFib(b, c, n-1);
+//     }
+//     public static void main(String args[]){
+//         int a = 0, b = 1;
+//         System.out.print(a+","+b+",");
+//         // System.out.println(b);
+//         int n = 7;  //this is how many terms you want to print
+//         printFib(a,b,n-2);  //here we put the n-2 because we have already used the 2 terms    
+//     }
+// }
+
+
+// -------------------------------------------------
+// print x^n (stack height = n)
+public class Recursion2{
+    public static void printFib(int a, int b, int n) {
+
+    }
+    public static void main(String args[]){
+
+    }
+}
