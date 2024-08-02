258c9ff115c35b9e893ad458bc2437685271f990
hiteshpakhan <hitesh5pakhan@gmail.com>
2024-08-02 12:02:22 +0530

    a

---
+++ Recursion2.java
@@ -0,0 +1,49 @@
+// --------------------------------
+// // here we have print the 5-1
+// public class Recursion2{
+
+//     public static void printNumbers(int n) {
+//         if(n == 0) {
+//             return;
+//         }   
+    
+//         System.out.println(n);
+//         printNumbers(n-1);
+//     }
+
+//     public static void main(String args[]){
+//         int n = 5;
+//         printNumbers(n);
+//     }
+
+// }
+// // output
+// // 5
+// // 4
+// // 3
+// // 2
+// // 1
+
+
+
+// --------------------------------------
+
+// //here we will print the 1-5 by using recursion
+// public class Recursion2{
+
+//     public static void printNumbers(int n) {
+//         if(n == 6) {
+//             return;
+//         }   
+    
+//         System.out.println(n);
+//         printNumbers(n+1);
+//     }
+//     public static void main(String args[]){
+//         int n = 1;
+//         printNumbers(n);
+//     }
+// }
+
+//-------------------------------------------------
+
